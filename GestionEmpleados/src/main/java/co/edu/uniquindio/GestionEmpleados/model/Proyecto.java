144f582049cc83b9fd785b6f493bd1d794fdec30
jharpin <154296932+jharpin@users.noreply.github.com>
2025-02-26 23:54:34 -0500

    version final proyecto

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
@@ -1,8 +1,10 @@
 package co.edu.uniquindio.GestionEmpleados.model;
+import java.util.ArrayList;
 
 public class Proyecto {
     private String nombreProyecto;
     private String codigoProyecto;
+    private ArrayList<Empleado>Empleados=new ArrayList();
 
     public Proyecto(String nombreProyecto, String codigoProyecto) {
         this.nombreProyecto=nombreProyecto;
@@ -19,6 +21,12 @@ public class Proyecto {
     }
     public void setCodigoProyecto(String codigoProyecto){
         this.codigoProyecto=codigoProyecto;
+    }
+    public ArrayList<Empleado> getEmpleados(){
+        return Empleados;
+    }
+    public void setEmpleados(ArrayList<Empleado> empleados){
+        this.Empleados=Empleados;
     }
 
 }
