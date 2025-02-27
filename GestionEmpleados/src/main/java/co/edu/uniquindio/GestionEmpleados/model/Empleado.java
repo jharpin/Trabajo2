fb0394e5244c8f8e18256b43a3b608ff38801ca4
Manuela P <Manuela.puertaa@uqvirtual.edu.co>
2025-02-26 23:55:31 -0500

    atributos empleado gerente y tecnico
    atributos empleado, los cuales son heredados por gerente y tecnico

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Empleado.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Empleado.java
@@ -4,5 +4,11 @@ public class Empleado {
     private String nombreEmpleado;
     private String idempleado;
     private String rolEspecifico;
+    public Empleado (String nombreEmpleado, String idempleado, String rolEspecifico) {
 
+        this.nombreEmpleado = nombreEmpleado;
+        this.idempleado = idempleado;
+        this.rolEspecifico = rolEspecifico;
+
+    }
 }
