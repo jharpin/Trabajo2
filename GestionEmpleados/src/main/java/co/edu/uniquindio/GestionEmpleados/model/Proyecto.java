e73773b20a1e34d5e836b5a43f8c0feef5199b63
jharpin <154296932+jharpin@users.noreply.github.com>
2025-02-26 23:50:44 -0500

    clase proyecto
    V:

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
@@ -3,4 +3,22 @@ package co.edu.uniquindio.GestionEmpleados.model;
 public class Proyecto {
     private String nombreProyecto;
     private String codigoProyecto;
+
+    public Proyecto(String nombreProyecto, String codigoProyecto) {
+        this.nombreProyecto=nombreProyecto;
+        this.codigoProyecto=codigoProyecto;
+    }
+    public String getNombreProyecto(){
+        return nombreProyecto;
+    }
+    public void setNombreProyecto(String nombreProyecto){
+        this.nombreProyecto=nombreProyecto;
+    }
+    public String getCodigoProyecto(){
+        return codigoProyecto;
+    }
+    public void setCodigoProyecto(String codigoProyecto){
+        this.codigoProyecto=codigoProyecto;
+    }
+
 }
