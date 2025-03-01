138ae6c1f0f82a3c710ba4a91b43a616b237ea75
Manuela P <Manuela.puertaa@uqvirtual.edu.co>
2025-03-01 01:29:55 -0500

    getandsetters

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Empleado.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Empleado.java
@@ -1,14 +1,19 @@
 package co.edu.uniquindio.GestionEmpleados.model;
+import java.util.ArrayList;
+
+import java.util.ArrayList;
 
 public class Empleado {
-    private String nombreEmpleado;
-    private String idempleado;
+    String nombreEmpleado;
+    String idempleado;
     private String rolEspecifico;
+
     public Empleado (String nombreEmpleado, String idempleado, String rolEspecifico) {
 
         this.nombreEmpleado = nombreEmpleado;
         this.idempleado = idempleado;
         this.rolEspecifico = rolEspecifico;
+
 
     }
 }
--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Gerente.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Gerente.java
@@ -1,9 +1,20 @@
 package co.edu.uniquindio.GestionEmpleados.model;
 
-public class Gerente extends Empleado {
+import java.util.ArrayList;
 
+public class Gerente extends Empleado {
+    private ArrayList<Gerente>Gerente=new ArrayList();
 
     public Gerente(String nombreEmpleado, String idempleado, String rolEspecifico) {
         super(nombreEmpleado, idempleado, rolEspecifico);
     }
-}
+
+        public String nombreEmpleado() {return nombreEmpleado();}
+        public void setNombreEmpleado(String nombreEmpleado) {this.nombreEmpleado=nombreEmpleado;}
+        public String getidempleado(){return idempleado;}
+        public void setidempleado(String idempleado){this.idempleado=idempleado;}
+        public ArrayList getGerente(){return Gerente;}
+        public void setGerente(ArrayList<Gerente> gerentes){this.Gerente=Gerente;
+        }
+    }
+
