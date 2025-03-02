2c767f9173f068e7e9f746c0b19d28e9378b971d
SVEGA30 <159830051+SVEGA30@users.noreply.github.com>
2025-03-02 16:07:15 -0500

    IDepartamentoCrud
    metodos

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/IDepartamentoCrud.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/IDepartamentoCrud.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.GestionEmpleados.services;
 
 public interface IDepartamentoCrud {
+    boolean crearDepartamento(String nombre,String codigo);
+    boolean actualizarDepartamento(String nombre,String codigo, String codigoNuevo);
+    boolean eliminarDepartamento(String codigo);
+    String obtenerDatosDepartamento(String codigo);
     
 }
