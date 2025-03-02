b84f45e08c5b28218d1cc25f8e062721c1f62342
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 18:17:41 -0500

    cambios

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
@@ -1,6 +1,20 @@
 package co.edu.uniquindio.GestionEmpleados;
+
+import co.edu.uniquindio.GestionEmpleados.factory.ModelFactory;
+import co.edu.uniquindio.GestionEmpleados.model.Empleado;
+
 public class Main {
     public static void main(String[] args) {
-
+        ModelFactory modelFactory=ModelFactory.getInstance();
+        crudEmpleado(modelFactory);
     }
+
+    private static void crudEmpleado(ModelFactory modelFactory) {
+        crearPropietario(modelFactory);
+    }
+    private static void crearPropietario(ModelFactory modelFactory) {
+        Empleado empleado=new Empleado("vega","12345",null);
+        modelFactory.crearEmpleado(empleado);
+    }
+
 }
--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/factory/ModelFactory.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/factory/ModelFactory.java
@@ -1,16 +1,19 @@
 package co.edu.uniquindio.GestionEmpleados.factory;
 
+import co.edu.uniquindio.GestionEmpleados.model.SistemaGestionEmpresa;
+import co.edu.uniquindio.GestionEmpleados.model.Empleado;
+import co.edu.uniquindio.GestionEmpleados.services.IEmpleadoCrud;
 import co.edu.uniquindio.GestionEmpleados.services.ITecnicoCrud;
 import co.edu.uniquindio.GestionEmpleados.services.ImodelFactoryServices;
-import co.edu.uniquindio.GestionEmpleados.model.SistemaGestionEmpresa;
 import co.edu.uniquindio.GestionEmpleados.services.IDepartamentoCrud;
 
-public class ModelFactory {
+public class ModelFactory implements IEmpleadoCrud {
     private static ModelFactory modelFactory;
     private SistemaGestionEmpresa  sistemaGestionEmpresa;
 
     private ModelFactory(){
         inicializarDatos();
+
     }
     public static ModelFactory getInstance(){
         if (modelFactory == null){
@@ -19,7 +22,12 @@ public class ModelFactory {
         return  modelFactory;
     }
     private void inicializarDatos() {
+
     }
 
 
+    @Override
+    public boolean crearEmpleado(Empleado empleado) {
+        return sistemaGestionEmpresa.crearEmpleado(empleado);
+    }
 }
