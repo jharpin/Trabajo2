a6fc93341af07877051082a64eb2ef9c2e1f4d9d
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 14:41:42 -0500

    proyecto cambios

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Proyecto.java
@@ -1,25 +1,38 @@
 package co.edu.uniquindio.GestionEmpleados.model;
 import java.util.ArrayList;
-import java.util.List;
 public class Proyecto {
     private String nombreProyecto;
     private String codigoProyecto;
-    private List<Empleado> empleadosAsociado;
+    private ArrayList<Empleado> ListaEmpleadosAsignados;
+    private ArrayList<Departamento>ListaDepartamentos;
     public Proyecto(String nombre, String codigo) {
         this.nombreProyecto = nombre;
         this.codigoProyecto = codigo;
-        this.empleadosAsociado = new ArrayList<>();
-    }
-    public Proyecto(){
-    }
-    public List<Empleado> getEmpleadosAsignados() {
-        return empleadosAsociado;
+        this.ListaEmpleadosAsignados = new ArrayList<>();
+        this.ListaDepartamentos = new ArrayList<>();
     }
     public String getNombreProyecto() {
         return nombreProyecto;
     }
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
     public String getCodigoProyecto() {
         return codigoProyecto;
     }
- 
+    public void setCodigoProyecto(String codigoProyecto) {
+        this.codigoProyecto = codigoProyecto;
+    }
+    public ArrayList<Empleado> getListaEmpleadosAsignados() {
+        return ListaEmpleadosAsignados;
+    }
+    public void setListaEmpleadosAsignados(){
+        this.ListaEmpleadosAsignados = new ArrayList<>();
+    }
+    public ArrayList<Departamento> getListaDepartamentos() {
+        return ListaDepartamentos;
+    }
+    public void setListaDepartamentos(){
+        this.ListaDepartamentos = new ArrayList<>();
+    }
 }
