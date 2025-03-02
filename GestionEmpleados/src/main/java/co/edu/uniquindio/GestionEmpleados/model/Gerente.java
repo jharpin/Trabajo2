fc1091c0529dd0e34bfd597da3f23144218667b1
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 14:59:32 -0500

    Clases:gerente/tecnico
    se les agrgearon la asociacion con deparamento

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Gerente.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Gerente.java
@@ -1,7 +1,18 @@
 package co.edu.uniquindio.GestionEmpleados.model;
 public class Gerente extends Empleado {
-    public Gerente(String nombre, String id,Proyecto proyectoAsociado ) {
+    private Departamento departamentoAsociado;
+    public Gerente(String nombre,
+                   String id,
+                   Proyecto proyectoAsociado,
+                   Departamento departamentoAsociado) {
         super(nombre, id,proyectoAsociado);
+        this.departamentoAsociado = departamentoAsociado;
+    }
+    public Departamento getDepartamentoAsociado() {
+        return departamentoAsociado;
+    }
+    public void setDepartamentoAsociado(Departamento departamentoAsociado) {
+        this.departamentoAsociado = departamentoAsociado;
     }
 }
 
--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Tecnico.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Tecnico.java
@@ -1,7 +1,14 @@
 package co.edu.uniquindio.GestionEmpleados.model;
 public class Tecnico extends Empleado {
-    public Tecnico(String nombre, String id,Proyecto proyectoAsociado) {
+    private Departamento departamentoAsociado;
+    public Tecnico(String nombre, String id,Proyecto proyectoAsociado,Departamento departamentoAsociado) {
         super(nombre, id,proyectoAsociado);
+        this.departamentoAsociado = departamentoAsociado;
     }
-
+    public Departamento getDepartamentoAsociado() {
+        return departamentoAsociado;
+    }
+    public void setDepartamentoAsociado(Departamento departamentoAsociado) {
+        this.departamentoAsociado = departamentoAsociado;
+    }
 }
