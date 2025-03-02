25f22065afdb547ed51212cb7593738e3500cc9c
SVEGA30 <159830051+SVEGA30@users.noreply.github.com>
2025-03-02 15:33:20 -0500

    ClaseTecnico
    Contribuir( )

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Tecnico.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/Tecnico.java
@@ -1,6 +1,12 @@
 package co.edu.uniquindio.GestionEmpleados.model;
 public class Tecnico extends Empleado {
     private Departamento departamentoAsociado;
+    private String especialidad;
+    public Tecnico(String nombre, String id,Proyecto proyectoAsociado,Departamento departamentoAsociado,String especialidad) {
+        super(nombre, id,proyectoAsociado);
+        this.departamentoAsociado = departamentoAsociado;
+        this.especialidad = especialidad;
+    }
     public Tecnico(String nombre, String id,Proyecto proyectoAsociado,Departamento departamentoAsociado) {
         super(nombre, id,proyectoAsociado);
         this.departamentoAsociado = departamentoAsociado;
@@ -11,4 +17,15 @@ public class Tecnico extends Empleado {
     public void setDepartamentoAsociado(Departamento departamentoAsociado) {
         this.departamentoAsociado = departamentoAsociado;
     }
+    public String getEspecialidad() {
+        return especialidad;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    @Override
+    public String contribuir() {
+        return "El técnico " + getNombre() + " contribuye con su especialidad en: " + especialidad;
+    }
+
 }
