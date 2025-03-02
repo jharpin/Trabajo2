ee93b98075209bd215f1cdc9b363352c270d160e
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 17:21:55 -0500

    cambios

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
@@ -1,18 +1,22 @@
 package co.edu.uniquindio.GestionEmpleados.model;
+import co.edu.uniquindio.GestionEmpleados.services.IEmpleadoCrud;
+
 import java.util.ArrayList;
-public class SistemaGestionEmpresa implements IEmpleadoCrud{
+public class SistemaGestionEmpresa implements IEmpleadoCrud {
     private ArrayList<Tecnico> tecnicos;
     private ArrayList<Gerente> gerentes;    
     private ArrayList<Departamento> departamentos;
     private ArrayList<Proyecto> proyectos;
-    @Override
-    public boolean CrearEmpleado(Empleado empleado){
-        return false;
-    }
+
     public SistemaGestionEmpresa() {
         this.tecnicos = new ArrayList<>();
         this.gerentes = new ArrayList<>();
         this.departamentos = new ArrayList<>();
         this.proyectos = new ArrayList<>();
+    }
+
+    @Override
+    public boolean crearEmpleado(Empleado empleado) {
+        return false;
     }
 }
