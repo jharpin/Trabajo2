ac11d2fb0afd7575ff86cafe50bc70641027e5af
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 17:10:34 -0500

    IEmpleadoCrud

---
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/IEmpleadoCrud.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.GestionEmpleados.services;
+
+import co.edu.uniquindio.GestionEmpleados.model.Empleado;
+
+public interface IEmpleadoCrud {
+    boolean agregarEmpleado(Empleado empleado);
+}
