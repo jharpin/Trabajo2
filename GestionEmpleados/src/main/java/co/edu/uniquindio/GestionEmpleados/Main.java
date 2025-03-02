a45cd376bc61843f45902d00e6096561cefd9a9a
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-02 11:37:16 -0500

    limpieza

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
@@ -7,23 +7,6 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        while (true) {
-            System.out.println("\n--- Sistema de Gestión de Empleados ---");
-            System.out.println("1. Crear Empleado");
-            System.out.println("2. Listar Empleados");
-            System.out.println("3. Crear Departamento");
-            System.out.println("4. Listar Departamentos");
-            System.out.println("5. Crear Proyecto");
-            System.out.println("6. Listar Proyectos");
-            System.out.println("7. Asignar Empleado a Proyecto");
-            System.out.println("8. Salir");
-            System.out.print("Seleccione una opción: ");
-            int opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
 
-
-        }
-        }
     }
+}
