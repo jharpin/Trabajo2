380a5b8100048e25ce9d97c26a55f88dc48fc8b2
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-03 12:00:51 -0500

    correciones

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
@@ -4,6 +4,7 @@ import co.edu.uniquindio.GestionEmpleados.services.IEmpleadoCrud;
 import co.edu.uniquindio.GestionEmpleados.services.IGerenteCrud;
 import java.util.ArrayList;
 public class SistemaGestionEmpresa implements IEmpleadoCrud{
+    private ArrayList<Empleado> Empleados;
     private ArrayList<Tecnico> tecnicos;
     private ArrayList<Gerente> gerentes;    
     private ArrayList<Departamento> departamentos;
@@ -15,23 +16,41 @@ public class SistemaGestionEmpresa implements IEmpleadoCrud{
         this.departamentos = new ArrayList<>();
         this.proyectos = new ArrayList<>();
     }
+    public ArrayList<Empleado>getEmpleados(){
+        return Empleados;
+    }
+    public void setEmpleados(ArrayList<Empleado> empleados){
+        this.Empleados = empleados;
+    }
+    public ArrayList<Tecnico> getTecnicos() {
+        return tecnicos;
+    }
+    public void setTecnicos(ArrayList<Tecnico> tecnicos) {
+        this.tecnicos = tecnicos;
+    }
+    public ArrayList<Gerente> getGerentes() {
+        return gerentes;
+    }
+    public void setGerentes(ArrayList<Gerente> gerentes) {
+        this.gerentes = gerentes;
+    }
     public ArrayList<Departamento> getDepartamentos() {
         return departamentos;
     }
-
-    public ArrayList<Empleado> empleados;
-
-    public ArrayList<Empleado> getEmpleados() {
-        return empleados;
+    public void setDepartamentos(ArrayList<Departamento> departamentos) {
+        this.departamentos = departamentos;
     }
+    public ArrayList<Proyecto> getProyectos() {
+        return proyectos;
+    }
+    public void setProyectos(ArrayList<Proyecto> proyectos) {
+        this.proyectos = proyectos;
+    }
+
     @Override
     public boolean crearEmpleado(String nombre, String id, Proyecto proyectoAsociado) {
         return false;
     }
-    public ArrayList<Departamento> getGerentes() {
-        throw new UnsupportedOperationException("");
-    }
-    public ArrayList<Departamento> getTecnicos() {
-        throw new UnsupportedOperationException("");
-    }
-}   
+
+}
+
