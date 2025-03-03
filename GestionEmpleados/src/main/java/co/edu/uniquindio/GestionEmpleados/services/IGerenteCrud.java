3d0509e95f1098f379928ab05c61ba753fb1e1b5
SVEGA30 <159830051+SVEGA30@users.noreply.github.com>
2025-03-03 14:54:48 -0500

    IGerenteCrud
    metodos

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/IGerenteCrud.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/IGerenteCrud.java
@@ -1,5 +1,14 @@
 package co.edu.uniquindio.GestionEmpleados.services;
-public interface IGerenteCrud {
-    boolean addgerente(String nombreEmpleado, String idempleado, String rolEspecifico);
+import co.edu.uniquindio.GestionEmpleados.model.Departamento;
+import co.edu.uniquindio.GestionEmpleados.model.Gerente;
+import co.edu.uniquindio.GestionEmpleados.model.Proyecto;
 
+public interface IGerenteCrud {
+    boolean creargerente(String nombre, String id, 
+    Proyecto proyectoAsociado,Departamento departamentoAsociado);
+    boolean actualizargerente(String nombre, String id, 
+    Proyecto proyectoAsociado, Departamento departamentoAsociado,
+     String idNuevo);
+    boolean eliminargerente(String id);
+    Gerente obtenergerente(String id);
 }
