849f595a45358789ecbc765daff431ef15b26b32
Manuela P <Manuela.puertaa@uqvirtual.edu.co>
2025-03-03 12:32:59 -0500

    commitdev sistem

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ModelFactory modelFactory=ModelFactory.getInstance();
         crudEmpleado(modelFactory);
+        modelFactory.mostrarInfoEmpresa();
     }
 
 
--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/model/SistemaGestionEmpresa.java
@@ -11,6 +11,7 @@ public class SistemaGestionEmpresa implements IEmpleadoCrud{
     private ArrayList<Proyecto> proyectos;
 
     public SistemaGestionEmpresa() {
+        this.Empleados= new ArrayList<>();
         this.tecnicos = new ArrayList<>();
         this.gerentes = new ArrayList<>();
         this.departamentos = new ArrayList<>();
