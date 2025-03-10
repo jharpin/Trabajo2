573b94c3f9b45f6039982c20693092aac8530c7e
jharpin <154296932+jharpin@users.noreply.github.com>
2025-03-10 12:41:39 -0500

    cambios

--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/Main.java
@@ -3,43 +3,15 @@ import co.edu.uniquindio.GestionEmpleados.factory.ModelFactory;
 public class Main {
     public static void main(String[] args) {
         ModelFactory modelFactory = ModelFactory.getInstance();
-        modelFactory.crearProyecto("software desing ","y878",null,null);
-        /// se inicializa crud proyecto
+        crearEmpleado(modelFactory);
         modelFactory.mostrarInfoEmpresa();
-        /// se inicializa crud empleado
-        modelFactory.crearEmpleado("vega2","450",null);
-        modelFactory.eliminarEmpleado("1011");
-        modelFactory.ActualizarEmpleado("5105","pepito",null);
-
-        /// se inicializa crud proyecto
-        modelFactory.crearProyecto("manufactura","JKL",null,null);
-        modelFactory.eliminarProyecto("1039");
-        modelFactory.modificarProyecto("peliculas","6578",null,null);
-
-        /// se inicializa crud Gerente
-        modelFactory.crearGerente("manu", "mnñ", null, null);
-        modelFactory.eliminarGerente("6025");
-        modelFactory.actualizarGerente("sans","6868",null,null);
-
-        ///ejercicio propio1
-        modelFactory.agregarProyectoAsociadaGerente("mnñ");
-        /// se inicializa crud tecnico
-        modelFactory.crearTecnico("Franco", "456", null, null, null);
-        modelFactory.actualizarTecnico("Romario", "6025", null, null, "carpintero");
-        modelFactory.eliminarTecnico("5105");
-
-
-        /// se inicializa crud departamento
-        modelFactory.crearDepartamento("ingenieria ","152201",null,null,null);
-        modelFactory.eliminarDepartamento("6060");
-        modelFactory.actualizarDepartamento("comida","2020",null,null,null);
-        modelFactory.mostrarInfoEmpresa();
-        ///ejercicio propio2
-        modelFactory.listarGerentesYProyectos();
-        modelFactory.tecnicoPerteneceProyecto();
-
     }
+    private static void crearEmpleado(ModelFactory modelFactory) {
+        Boolean resultado=modelFactory.crearEmpleado("juan","mnb",null);
+
+        }
     }
+
 
 
 
--- GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/ImodelFactoryServices.java
+++ GestionEmpleados/src/main/java/co/edu/uniquindio/GestionEmpleados/services/ImodelFactoryServices.java
@@ -3,7 +3,8 @@ package co.edu.uniquindio.GestionEmpleados.services;
 import co.edu.uniquindio.GestionEmpleados.model.Empleado;
 import co.edu.uniquindio.GestionEmpleados.model.Proyecto;
 
-public interface ImodelFactoryServices extends IEmpleadoCrud, IDepartamentoCrud {
+public interface ImodelFactoryServices extends IEmpleadoCrud,
+        IDepartamentoCrud,IGerenteCrud,ITecnicoCrud,IProyectoCrud {
     void mostrarInfoEmpresa();
 
     boolean agregarProyectoAsociadaGerente(String id);
